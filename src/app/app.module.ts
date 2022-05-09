0235e9d5b080a7d254fceee81da15b39a4c8ab75
ahmedhawash <ahmed.hawash11@hotmail.com>
2022-05-09 09:58:30 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatToolbarModule, MatIconModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
